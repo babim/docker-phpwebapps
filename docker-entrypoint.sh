3d3726523f9129bfcf8cadfcec25dbaf1cb0a950
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-02-16 13:59:46 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ ! -e '/var/www/html/index.php' ]; then
-	tar cf - --one-file-system -C /usr/src/smf . | tar xf -
+	unzip /usr/src/smf.zip -d /var/www/html
 	chown -R www-data /var/www/html
 fi
 
