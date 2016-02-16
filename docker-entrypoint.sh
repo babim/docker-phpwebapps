e6a6e7e98318193f540f607ca1772e1b535f25af
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-02-16 11:39:57 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 if [ ! -e '/var/www/html/index.php' ]; then
 	rm -rf /var/www/html
-	unzip /usr/src/prestashop.zip -C /var/www/
-	mv /var/www/prestashop /var/www/html
+	unzip /usr/src/prestashop.zip
+	mv /usr/src/prestashop /var/www/html
 	chown -R www-data /var/www/html
 fi
 
