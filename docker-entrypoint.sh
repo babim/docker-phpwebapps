ecac48c92571337135390117c317beecccaef792
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-02-16 12:55:37 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,6 @@ if [ ! -e '/var/www/html/wp-login.php' ]; then
 	tar xvf /usr/src/wordpress.tar.gz -C /var/www/
 	mv /var/www/wordpress /var/www/html
 	chown -R www-data /var/www/html
+fi
 
 exec "$@"
