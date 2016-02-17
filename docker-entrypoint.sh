829fb08258665a777fe10eec06329f56da59af65
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-02-17 12:23:48 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+if [ -z "`ls /usr/local/etc/php/conf.d`" ] 
+then
+	cp -R /etc-start/php/conf.d/ /usr/local/etc/php/conf.d
+fi
 if [ ! -e '/var/www/html/index.php' ]; then
 	unzip /usr/src/prestashop.zip -d /var/www/
 	mv /var/www/prestashop/ /var/www/html/ && mv /var/www/prestashop/.* /var/www/html/
