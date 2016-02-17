bac6a86f79cec8e00754558d2037654d3620bf82
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-02-17 12:23:23 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+if [ -z "`ls /usr/local/etc/php/conf.d`" ] 
+then
+	cp -R /etc-start/php/conf.d/ /usr/local/etc/php/conf.d
+fi
 if [ ! -e '/var/www/html/index.php' ]; then
 	unzip /usr/src/joomla.zip -d /var/www/html
 	chown -R www-data /var/www/html
