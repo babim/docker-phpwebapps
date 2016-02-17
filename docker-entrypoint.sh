b1373ea2494af2ae4942abbf4a05830c9d513b6b
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-02-17 12:23:00 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,9 @@ if [ ! -e '/var/www/html/wp-login.php' ]; then
 	mv /var/www/wordpress/ /var/www/html/ && mv /var/www/wordpress/.* /var/www/html/
 	chown -R www-data /var/www/html
 fi
+if [ -z "`ls /usr/local/etc/php/conf.d`" ] 
+then
+	cp -R /etc-start/php/conf.d/ /usr/local/etc/php/conf.d
+fi
 
 exec "$@"
