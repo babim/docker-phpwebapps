706cc94047b6479825cd176f39468c853c0dc643
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-02-17 12:24:08 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
+if [ -z "`ls /usr/local/etc/php/conf.d`" ] 
+then
+	cp -R /etc-start/php/conf.d/ /usr/local/etc/php/conf.d
+fi
 if [ ! -e '/var/www/html/wp-login.php' ]; then
 	tar xvf /usr/src/wordpress.tar.gz -C /var/www/
 	mv /var/www/wordpress/ /var/www/html/ && mv /var/www/wordpress/.* /var/www/html/
 	chown -R www-data /var/www/html
-fi
-if [ -z "`ls /usr/local/etc/php/conf.d`" ] 
-then
-	cp -R /etc-start/php/conf.d/ /usr/local/etc/php/conf.d
 fi
 
 exec "$@"
