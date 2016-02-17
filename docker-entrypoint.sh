849e243faf7e23ced8752de2f90c6349d2843ef5
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-02-17 12:23:32 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+if [ -z "`ls /usr/local/etc/php/conf.d`" ] 
+then
+	cp -R /etc-start/php/conf.d/ /usr/local/etc/php/conf.d
+fi
 if [ ! -e '/var/www/html/version.php' ]; then
 	tar cf - --one-file-system -C /usr/src/owncloud . | tar xf -
 	chown -R www-data /var/www/html
