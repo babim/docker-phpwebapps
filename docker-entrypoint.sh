22d687ed804a08b68d08defce901b9af23a0ba00
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-12-10 19:07:02 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,10 +5,24 @@ if [ -z "`ls /usr/local/etc/php/conf.d`" ]
 then
 	cp -R /etc-start/php/conf.d/* /usr/local/etc/php/conf.d
 fi
+
+# set ID docker run
+agid=${agid:-$auid}
+auser=www-data
+
+if [[ -z "${auid}" ]]; then
+  echo "start"
+elif [[ "$auid" = "0" ]] || [[ "$aguid" == "0" ]]; then
+ echo "can't run in Root user. Default user still run."
+else
+  usermod -u $auid $auser
+  groupmod -g $agid $auser
+fi
+
 if [ ! -e '/var/www/html/index.php' ]; then
 	tar xvf /usr/src/drupal.tar.gz -C /var/www/
 	mv /var/www/drupal*/ /var/www/html/ && mv /var/www/drupal*/.* /var/www/html/
-	chown -R www-data /var/www/html
+	chown -R $auser:$auser /var/www/html
 fi
 
 exec "$@"
