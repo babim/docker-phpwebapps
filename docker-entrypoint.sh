3dd24fd5239a9cb45a8c2e30c86f297cf4e2fcab
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-12-10 19:01:21 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -22,7 +22,7 @@ fi
 if [ ! -e '/var/www/html/index.php' ]; then
 	tar xvf /usr/src/drupal.tar.gz -C /var/www/
 	mv /var/www/drupal*/ /var/www/html/ && mv /var/www/drupal*/.* /var/www/html/
-	chown -R www-data /var/www/html
+	chown -R $auser:$auser /var/www/html
 fi
 
 exec "$@"
