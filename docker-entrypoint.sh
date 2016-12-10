26b0c0d6353bc84225712d9379b2239579f59d01
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-12-10 19:03:24 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,10 +5,6 @@ if [ -z "`ls /usr/local/etc/php/conf.d`" ]
 then
 	cp -R /etc-start/php/conf.d/* /usr/local/etc/php/conf.d
 fi
-if [ ! -e '/var/www/html/version.php' ]; then
-	tar cf - --one-file-system -C /usr/src/owncloud . | tar xf -
-	chown -R www-data /var/www/html
-fi
 
 # set ID docker run
 agid=${agid:-$auid}
@@ -21,6 +17,11 @@ elif [[ "$auid" = "0" ]] || [[ "$aguid" == "0" ]]; then
 else
   usermod -u $auid $auser
   groupmod -g $agid $auser
+fi
+
+if [ ! -e '/var/www/html/version.php' ]; then
+	tar cf - --one-file-system -C /usr/src/owncloud . | tar xf -
+	chown -R $auser:$auser /var/www/html
 fi
 
 exec "$@"
