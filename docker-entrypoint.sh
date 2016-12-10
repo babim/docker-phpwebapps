ef63c61e26784c179b9dc1761afc45c772dc6076
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-12-10 19:02:59 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -21,7 +21,7 @@ fi
 
 if [ ! -e '/var/www/html/index.php' ]; then
 	unzip /usr/src/joomla.zip -d /var/www/html
-	chown -R www-data /var/www/html
+	chown -R $auser:$auser /var/www/html
 fi
 
 exec "$@"
