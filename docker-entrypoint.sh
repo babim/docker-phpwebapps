8343f1903df9be038077e2cd949448f4985f241e
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-12-11 10:57:42 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 set -e
 
-if [ -z "`ls /usr/local/etc/php/conf.d`" ] 
-then
-	cp -R /etc-start/php/conf.d/* /usr/local/etc/php/conf.d
-fi
-
-# set ID docker run
-agid=${agid:-$auid}
-auser=www-data
-
-if [[ -z "${auid}" ]]; then
-  echo "start"
-elif [[ "$auid" = "0" ]] || [[ "$aguid" == "0" ]]; then
- echo "can't run in Root user. Default user still run."
-else
-  usermod -u $auid $auser
-  groupmod -g $agid $auser
+if [ "$1" = '/run.sh' ]; then
+	exec /run.sh "$@"
 fi
 
 if [ ! -e '/var/www/html/index.php' ]; then
