c3e5e843ee83320ff07b21c690562f1e7ea4ee81
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-12-11 10:30:49 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,17 +6,63 @@ then
 	cp -R /etc-start/php/conf.d/* /usr/local/etc/php/conf.d
 fi
 
+   # Set environments
+    TIMEZONE1=${TIMEZONE:-Asia/Ho_Chi_Minh}
+    PHP_MEMORY_LIMIT1=${PHP_MEMORY_LIMIT:-512M}
+    MAX_UPLOAD1=${MAX_UPLOAD:-520M}
+    PHP_MAX_FILE_UPLOAD1=${PHP_MAX_FILE_UPLOAD:-200}
+    PHP_MAX_POST1=${PHP_MAX_POST:-520M}
+    MAX_INPUT_TIME1=${MAX_INPUT_TIME:-3600}
+    MAX_EXECUTION_TIME1=${MAX_EXECUTION_TIME:-3600}
+	
+	sed -i -E \
+	-e "s|;*date.timezone =.*|date.timezone = ${TIMEZONE1}|i" \
+	-e "s|;*memory_limit =.*|memory_limit = ${MAX_UPLOAD1}|i" \
+ 	-e "s|;*upload_max_filesize =.*|upload_max_filesize = ${MAX_UPLOAD1}|i" \
+    	-e "s|;*max_file_uploads =.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD1}|i" \
+    	-e "s|;*post_max_size =.*|post_max_size = ${PHP_MAX_POST1}|i" \
+    	-e "s/max_input_time = 60/max_input_time = ${MAX_INPUT_TIME1}/" \
+	-e "s/max_execution_time = 30/max_execution_time = ${MAX_EXECUTION_TIME1}/" \
+	/usr/local/etc/php/conf.d/babim.ini
+
 # set ID docker run
 agid=${agid:-$auid}
-auser=www-data
+auser=${auser:-www-data}
 
 if [[ -z "${auid}" ]]; then
   echo "start"
 elif [[ "$auid" = "0" ]] || [[ "$aguid" == "0" ]]; then
- echo "can't run in Root user. Default user still run."
+	echo "run in user root"
+	auser=root
+	#export APACHE_RUN_USER=$auser
+	#export APACHE_RUN_GROUP=$auser
+	sed -i -e "/^user = .*/cuser = $auser" /usr/local/etc/php-fpm.conf
+	sed -i -e "/^group = .*/cgroup = $auser" /usr/local/etc/php-fpm.conf
 else
-  usermod -u $auid $auser
-  groupmod -g $agid $auser
+if id $auser >/dev/null 2>&1; then
+        echo "user exists"
+	sed -i -e "/^user = .*/cuser = $auser" /usr/local/etc/php-fpm.conf
+	sed -i -e "/^group = .*/cgroup = $auser" /usr/local/etc/php-fpm.conf
+	#export APACHE_RUN_USER=$auser
+	#export APACHE_RUN_GROUP=$auser
+	# usermod alpine
+		#deluser $auser && delgroup $auser
+		#addgroup -g $agid $auser && adduser -D -H -G $auser -s /bin/false -u $auid $auser
+	# usermod ubuntu/debian
+		usermod -u $auid $auser
+		groupmod -g $agid $auser
+else
+        echo "user does not exist"
+	#export APACHE_RUN_USER=$auser
+	#export APACHE_RUN_GROUP=$auser
+	# create user alpine
+	#addgroup -g $agid $auser && adduser -D -H -G $auser -s /bin/false -u $auid $auser
+	# create user ubuntu/debian
+	groupadd -g $agid $auser && useradd --system --uid $auid --shell /usr/sbin/nologin -g $auser $auser
+	sed -i -e "/^user = .*/cuser = $auser" /usr/local/etc/php-fpm.conf
+	sed -i -e "/^group = .*/cgroup = $auser" /usr/local/etc/php-fpm.conf
+fi
+
 fi
 
 if [ ! -e '/var/www/html/wp-login.php' ]; then
